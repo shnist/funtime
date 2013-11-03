6ed85c20f2628b5b3b66214a58c881a3841515b2
Aaron Faber <aaron.jack.faber@gmail.com>
2013-11-03 13:52:50 +0000

    Moved index into pages folder and broke out common elements into components folder

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,16 @@
 require.config({
 	baseUrl: 'src/js',
 	paths: {
-		'jquery': '../../assets/libs/jquery.1.10.2.min',
-		'bootstrap': '../../assets/libs/bootstrap.min',
-		'pubsub': '../../assets/libs/pubsub'
+		'jquery': '../../../assets/jquery.1.10.2.min',
+		'bootstrap': '../../../assets/bootstrap.min',
+		'pubsub': '../../../assets/pubsub'
 	},
 	shim: {
 		'bootstrap': {
 			deps: ['jquery']
 		},
 		'main': {
-			deps: ['jquery', 'bootstrap', 'index']
+			deps: ['jquery', 'bootstrap']
 		},
 		'pubsub': {
 			exports: 'pubsub'
